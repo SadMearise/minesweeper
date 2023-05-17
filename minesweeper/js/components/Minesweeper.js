04c04b7e9bf48ada605cd08de38ebe867764a623
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2023-05-17 17:33:08 +0300

    feat: add some functions

--- minesweeper/js/components/Minesweeper.js
+++ minesweeper/js/components/Minesweeper.js
@@ -16,7 +16,9 @@ export default class Minesweeper {
   }
 
   getStructure() {
-    const structure = {};
+    const structure = {
+      component: '',
+    };
 
     for (let i = 0; i < this.lines; i += 1) {
       const row = new BaseComponent({ classNames: ['board__row'] });
@@ -27,23 +29,48 @@ export default class Minesweeper {
         structure[`row${i}`][`cell${j}`] = { component: cell };
       }
     }
+
     return structure;
   }
 
+  endGame() { // lose or win option
+    console.log('end game');
+  }
+
+  getCountMinesAroundCell() {
+    const fullBoard = [];
+    console.log(this.mines);
+
+    for (let i = 0; i < this.lines; i += 1) {
+      fullBoard.push([]);
+      for (let j = 0; j < this.lines; j += 1) {
+        if (this.mines.includes(`${i}-${j}`)) fullBoard[i].push(1);
+        else fullBoard[i].push(0);
+      }
+    }
+    console.log(fullBoard);
+  }
+
   getMines() {
-    for (let i = 0; i < 25; i += 1) {
-      this.#pushIndexes();
+    for (let i = 0; i < this.countMines; i += 1) {
+      const ind = this.#getPairIndexes();
+      this.mines.push(ind);
     }
 
     return this.mines;
   }
 
-  #pushIndexes() {
+  replaceMine(index) {
+    this.mines[index] = this.#getPairIndexes();
+  }
+
+  #getPairIndexes() {
     const i = randomIndex(this.lines);
     const j = randomIndex(this.lines);
     const index = `${i}-${j}`;
 
-    if (this.mines.includes(index)) this.#pushIndexes();
-    else this.mines.push(index);
+    if (this.mines.includes(index)) return this.#getPairIndexes();
+
+    return index;
   }
 }
--- minesweeper/js/index.js
+++ minesweeper/js/index.js
@@ -2,16 +2,46 @@ import App from './components/App.js';
 import Minesweeper from './components/Minesweeper.js';
 import { initDOM } from './files/functions.js';
 
-const elements = App.getStructure();
+const appElements = App.getStructure();
+const { page } = appElements;
 
-const { page } = elements;
 const pageDOM = Object.assign(Object.create(Object.getPrototypeOf(page.component)), page.component);
 initDOM(page, pageDOM);
-document.body.prepend(pageDOM.getNode());
 
 const board = page.main.app.appCol2.appMinesweeper.minesweeperBoard.component;
 const minesweeperInstance = new Minesweeper(board);
 const minesweeperStructure = minesweeperInstance.getStructure();
 initDOM(minesweeperStructure, board);
 
+document.body.prepend(pageDOM.getNode());
+
 const minesArr = minesweeperInstance.getMines();
+
+const firstClick = true;
+
+// board.getNode().addEventListener('click', (event) => {
+//   const { target } = event;
+//   const cellPos = target.dataset.pos;
+//   if (minesArr.includes(cellPos)) { // наткнулись на мину
+//     if (firstClick) { // если 1 клик, то прощаем
+//       firstClick = false;
+//       const index = minesArr.indexOf(cellPos);
+//       minesweeperInstance.replaceMine(index);
+//     } else {
+//       minesweeperInstance.endGame();
+//     }
+//   } else { // не наткнулись на мину
+//     firstClick = false;
+//     // сюда вписываем алгоритм открытия поля
+//   }
+// });
+
+board.getNode().addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+  const { target } = event;
+
+  if (target.classList.value === 'board__cell cell') target.classList.add('cell_flag');
+  else if (target.classList.contains('cell_flag')) target.classList.remove('cell_flag');
+});
+
+minesweeperInstance.getCountMinesAroundCell();
